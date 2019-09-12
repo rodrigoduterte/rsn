f49add02cfcd9a52c1c4e1d12a440b6d42deef5f
DSBoyce <dboyce91@gmail.com>
2019-09-12 15:43:38 -0400

    updated Progress on Angular login, register, profile

---
+++ Angular/src/app/populate-feed.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { PostBean } from 'src/PostBean';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PopulateFeedService {
+
+  //existingPosts: Array<PostBean> = []
+
+  // fillFeed(jsonArrayOfPosts ){
+
+  //   for(let i=0 ; i<25 ; i++){
+  //    // this.existingPosts.push(jasonArray[i]); //need to define the input variable for the Recieved Json from DB 
+  //   }
+  // }
+  
+
+  constructor() { }
+}
