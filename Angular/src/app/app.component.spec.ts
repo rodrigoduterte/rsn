ce50bf44b2a382592d2c5e6ec1d597602d7ced35
Gabriel Ferrer <gabrielferrergtf@gmail.com>
2019-09-14 14:06:14 -0400

    Update app.component.spec.ts

--- Angular/src/app/app.component.spec.ts
+++ Angular/src/app/app.component.spec.ts
@@ -1,29 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import { RouterTestingModule } from '@angular/router/testing';
-=======
->>>>>>> Gael_Ngouana
-=======
-import { RouterTestingModule } from '@angular/router/testing';
->>>>>>> d9296d760cbbd49d4e7000258e2489d7fd58631d
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-<<<<<<< HEAD
-<<<<<<< HEAD
       imports: [
         RouterTestingModule
       ],
-=======
->>>>>>> Gael_Ngouana
-=======
-      imports: [
-        RouterTestingModule
-      ],
->>>>>>> d9296d760cbbd49d4e7000258e2489d7fd58631d
       declarations: [
         AppComponent
       ],
@@ -36,12 +22,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
   it(`should have as title 'Project2SocialNetwork'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Project2SocialNetwork');
+    expect(app.title).toEqual('spring-angular');
   });
 
   it('should render title', () => {
@@ -49,25 +33,5 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('Project2SocialNetwork app is running!');
-=======
-  it(`should have as title 'spring-angular'`, () => {
-=======
-  it(`should have as title 'Project2SocialNetwork'`, () => {
->>>>>>> d9296d760cbbd49d4e7000258e2489d7fd58631d
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('spring-angular');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-<<<<<<< HEAD
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to spring-angular!');
->>>>>>> Gael_Ngouana
-=======
-    expect(compiled.querySelector('.content span').textContent).toContain('Project2SocialNetwork app is running!');
->>>>>>> d9296d760cbbd49d4e7000258e2489d7fd58631d
   });
 });
