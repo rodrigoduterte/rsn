0d7591859d2ead145e65b7693380684070a0e634
DSBoyce <dboyce91@gmail.com>
2019-09-17 22:19:18 -0400

    updated Angular Code base

--- Angular/src/app/register/register.component.ts
+++ Angular/src/app/register/register.component.ts
@@ -19,12 +19,12 @@ export class RegisterComponent implements OnInit {
   constructor(private router:Router,private fb: FormBuilder, private _registrationService: RegistrationService) { }
   
   registrationForm = this.fb.group({
-  username: ['Username', [Validators.required, Validators.minLength(4)]],
-  password: ['Password', [Validators.required, Validators.minLength(4)]],
-  confirmPassword: ['Confirm Password', [Validators.required, Validators.minLength(4)]],
-  firstName: ['First Name', Validators.required],
-  lastName: ['Last Name',Validators.required ],
-  email: ['Email',Validators.required]
+  username: ['', [Validators.required, Validators.minLength(4)]],
+  password: ['', [Validators.required, Validators.minLength(4)]],
+  confirmPassword: ['', [Validators.required, Validators.minLength(4)]],
+  firstName: ['', Validators.required],
+  lastName: ['',Validators.required ],
+  email: ['',Validators.required]
 }, {validator: passwordMatchValidator});
 
 onSubmit(){
